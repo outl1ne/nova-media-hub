6fc3d6f5610fd5f57124d6a75c09049c5f79f397
Tarvo R <TarvoReinpalu@gmail.com>
2022-06-14 13:59:49 +0300

    WIP

--- resources/js/views/NovaMediaLibrary.vue
+++ resources/js/views/NovaMediaLibrary.vue
@@ -3,16 +3,18 @@
     <div class="nml-flex nml-border nml-full nml-border-slate-200 nml-rounded nml-bg-white nml-shadow">
       <!-- Collections list -->
       <div class="nml-flex nml-flex-col nml-border-r nml-border-slate-200">
-        <div class="nml-font-bold nml-border-b nml-border-slate-200 nml-px-6 nml-py-2">Collections</div>
+        <div class="nml-font-bold nml-border-b nml-border-slate-200 nml-px-6 nml-py-3 nml-text-center">Collections</div>
 
         <div class="nml-flex nml-flex-col">
-          <span>todo</span>
+          <div v-if="!collections.length" class="nml-text-sm nml-text-slate-400 nml-p-4 nml-whitespace-nowrap">
+            No collections found
+          </div>
         </div>
       </div>
 
       <!-- Media list -->
-      <div class="nml-flex nml-p-4">
-        <span>Media items</span>
+      <div class="nml-flex nml-p-4 nml-w-full" :class="{ 'nml-items-center nml-justify-center': !mediaItems.length }">
+        <div v-if="!mediaItems.length" class="nml-text-sm nml-text-slate-400">No media items found</div>
       </div>
     </div>
   </LoadingView>
@@ -22,6 +24,8 @@
 export default {
   data: () => ({
     loading: true,
+    collections: [],
+    mediaItems: [],
   }),
 
   mounted() {
