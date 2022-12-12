3bb6ea0064446381b48a6d4b3a24b0f35f5acf3d
Tarvo R <tarvoreinpalu@gmail.com>
2022-12-12 16:50:53 +0200

    Fix dark mode handling

--- resources/js/entry.js
+++ resources/js/entry.js
@@ -1,17 +1,20 @@
 import DetailMediaHubField from './fields/MediaField/DetailMediaHubField';
 import FormMediaHubField from './fields/MediaField/FormMediaHubField';
 
-Nova.booting((Vue, router, store) => {
-  new MutationObserver(() => {
-    const cls = document.documentElement.classList;
-    const isDarkMode = cls.contains('dark');
+const handleDarkMode = () => {
+  const cls = document.documentElement.classList;
+  const isDarkMode = cls.contains('dark');
 
-    if (isDarkMode && !cls.contains('o1-dark')) {
-      cls.add('o1-dark');
-    } else if (!isDarkMode && cls.contains('o1-dark')) {
-      cls.remove('o1-dark');
-    }
-  }).observe(document.documentElement, {
+  if (isDarkMode && !cls.contains('o1-dark')) {
+    cls.add('o1-dark');
+  } else if (!isDarkMode && cls.contains('o1-dark')) {
+    cls.remove('o1-dark');
+  }
+};
+
+Nova.booting((Vue, router, store) => {
+  handleDarkMode();
+  new MutationObserver(handleDarkMode).observe(document.documentElement, {
     attributes: true,
     attributeOldValue: true,
     attributeFilter: ['class'],
