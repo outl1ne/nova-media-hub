59253e99db51de842e97c29aeca53982fa738d33
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-02-25 23:08:27 -0500

    WIP: Use readonly field

--- resources/js/components/MediaViewModalInfoListItem.vue
+++ resources/js/components/MediaViewModalInfoListItem.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="o1-flex o1-flex-col o1-mb-2">
+  <div class="o1-flex o1-flex-col o1-mb-5">
     <label class="o1-leading-tight">{{ label }}</label>
-    <input
-      type="text"
-      class="o1-bg-slate-50 o1-border o1-border-slate-100 o1-rounded-md o1-px-2 o1-py-1 o1-whitespace-nowrap o1-text-slate-500 dark:o1-bg-slate-800 dark:o1-border-slate-700 dark:o1-text-slate-400 o1-overflow-x-auto"
-      :value="value"
-      :disabled="true"
-    />
+    <input type="text" class="w-full form-control form-input form-input-bordered" :value="value" :disabled="true" />
   </div>
 </template>
 
