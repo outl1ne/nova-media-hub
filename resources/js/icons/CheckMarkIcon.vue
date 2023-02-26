1020e6812ffb2d761e1327d1fd3bc8ed18323fc1
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-02-26 02:21:05 -0500

    Cleanup context-menu changes

--- resources/js/icons/CheckMarkIcon.vue
+++ resources/js/icons/CheckMarkIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-6 w-6 stroke-primary-500"
+    class="h-6 w-6 o1-stroke-primary-500"
     fill="none"
     viewBox="0 0 24 24"
     stroke-width="2"
