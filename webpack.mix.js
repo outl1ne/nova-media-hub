c0afe3a10cd0843c1b32522fbecb473f329d8639
Farid Aghili <faridaghili@hotmail.com>
2023-07-19 01:02:57 +0330

    add rtl support

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let path = require('path');
 let mix = require('laravel-mix');
 let postcss = require('postcss-import');
 let tailwindcss = require('tailwindcss');
+let postcssRtlcss = require('postcss-rtlcss');
 
 mix
   .setPublicPath('dist')
@@ -15,7 +16,7 @@ mix
       uniqueName: 'outl1ne/nova-media-hub',
     },
   })
-  .postCss('resources/css/entry.css', 'dist/css/', [postcss(), tailwindcss('tailwind.config.js')])
+  .postCss('resources/css/entry.css', 'dist/css/', [postcss(), tailwindcss('tailwind.config.js'), postcssRtlcss()])
   .alias({
     'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
   });
